e7c02b9387436dc7340f51836eed533fe913be26
Edsel Apostol <ed_apostol@yahoo.com>
2015-04-19 22:01:43 +0800

    [Refactor] Changed version number

--- uci.cpp
+++ uci.cpp
@@ -32,7 +32,7 @@
 const std::string Interface::name = "Hannibal";
 const std::string Interface::author = "Sam Hamilton & Edsel Apostol";
 const std::string Interface::year = "2015";
-const std::string Interface::version = "1.5";
+const std::string Interface::version = "1.6beta";
 const std::string Interface::arch = "x64";
 
 void Interface::Info() {
